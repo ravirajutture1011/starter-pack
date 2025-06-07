ce40379c5648e74ae82875517864162d86fa1f3f
ravirajutture1011 <rajutture1026@gmail.com>
2025-06-07 11:01:27 +0530

    redis testing done

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+
+import React, { useState } from "react";
+import { useUserStore } from "../stores/useUserStore";
+import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
+    const [formData, setFormData] = useState({
+      name: "",
+    }); 
+  
+   const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const{getData} = useUserStore();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const success = getData(formData);
+     
+    console.log("Submitted Data:", formData);
+
+  };
+
   return (
-    <div>SignupPage</div>
-  )
-}
+    <div>
+      <h2>GETDATA</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          id="name"
+          type="text"
+          required
+          value={formData.name} // ✅ Corrected this
+          onChange={onChange}
+          placeholder="Enter name here..."
+        />
+        <br />
+
+        <button type="submit" className="btn btn-success">Success</button>
+      </form>
+    </div>
+  );
+};
 
 export default SignupPage
+
+ 
--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -2,6 +2,10 @@ import { create } from "zustand";
 import { toast } from "react-hot-toast";
 import axiosInstance from "../libs/axios.js";
 
+
+
+
+
 export const useUserStore = create((set, get) => ({
   user: null,
   login: async ({name,password}) => {
@@ -13,4 +17,13 @@ export const useUserStore = create((set, get) => ({
       toast.error("Failed to login", { toastId: "uniqueToastId" });
     }
   },
+  getData : async({name})=>{
+    try {
+      const response = await axiosInstance.post('/getdata',{name});  
+      console.log("user dataaaa " + JSON.stringify(response.data, null, 2)); // ✅ formatted
+    }
+    catch (error) {
+      toast.error("Failed to get data", { toastId: "uniqueToastId" });
+    }
+  }
 }));
